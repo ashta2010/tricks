ce6987e783ab937f78ec1265714ee551e70d65cc
Mofah <good_boy3787@yahoo.com>
2018-04-01 12:05:17 +0200

    Update ideas.js

--- ideas.js
+++ ideas.js
@@ -1,19 +1,38 @@
 /*global $ , console , alert , document, window */
 $(function () {
   'use strict';
-
-
+  
+  
+  //auto-run Function
+  //auto-run Function
   (function () {
+  }()); 
 
-  }()); //auto-run Function
+  
+  
+  
+ //Check max Heigh in div and make all divs like it 
+ //Check max Heigh in div and make all divs like it   
+ var maxh = 0;
+    $('.container > div').each(function () {
+
+        if ($(this).height() > maxh) {
+
+            console.log( $(this).height());
+            maxh = $(this).height();
+        }
+        
+        maxh = $(this).height();
+    });  
+console.log('max H = ' + maxh);
+$('.container > div').height(maxh);
 
 
 
-
-
-
-
-
+  
+  
+  
+//Check if Div have not another div
 //Check if Div have not another div
   $("div").not(':has(anotherDiv)').remove();
 
